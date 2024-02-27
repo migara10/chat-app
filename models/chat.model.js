d6485af5bc8a0e388f33ab2fbd420190629735bc
unknown <migaraten@gmail.com>
2024-02-27 15:58:37 +0200

    add chat model

---
+++ models/chat.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const chatModel = mongoose.Schema(
+  {
+    chatName: { type: String, trim: true },
+    isGroupChat: { type: Boolean, default: false },
+    users: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    groupAdmin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+
+module.exports = mongoose.model("chatModel", chatModel)
