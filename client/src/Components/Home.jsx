6a239f2bcb9217ba94df7680d797ad2089d63c4f
unknown <migaraten@gmail.com>
2024-02-29 15:33:17 +0200

    add client modifications

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Login from './Login/Login';
+import Register from './Register/Register';
 
 function Home() {
+
+  const [isLogin, setIsLogin] = useState(true);
+
+  const changeIsLogin = () => {
+    setIsLogin(!isLogin)
+  }
+  
   return (
-    <div>Home</div>
+    <div>
+      {isLogin && <Login handleLogin={changeIsLogin} />}
+      {!isLogin && <Register handleLogin={changeIsLogin} />}
+    </div>
   )
 }
 
